238ae6efd1c210cac892c2c5c22b7b2b6f9ba359
fatima1221 <askerova.fatima1221@gmail.com>
2021-11-08 02:25:01 +0400

    switch active player

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 //Starting point
 scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let curScore = 0;
+let activePlayer = 0;
 
 //implementing functionality
 btnRoll.addEventListener("click", function () {
@@ -27,8 +31,20 @@ btnRoll.addEventListener("click", function () {
   //3 check if it is not one add to current score
   if (dice !== 1) {
     curScore += dice;
-    curEl0.textContent = curScore;
+    //building id name dynamicly
+    document.getElementById(`current--${activePlayer}`).textContent = curScore;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
+});
+
+btnRoll.addEventListener("click", function () {
+  //1. add current score to active player's score
+  //2. check if player's score is >=100
+  //if yes finish the game
+  //3. else switch to the next player
 });
